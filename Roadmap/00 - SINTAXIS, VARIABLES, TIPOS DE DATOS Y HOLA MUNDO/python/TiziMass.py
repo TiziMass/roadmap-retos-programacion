b53a48dd02e806f7059ce84bed651e8c01f7552b
TiziMass <149125722+TiziMass@users.noreply.github.com>
2024-01-03 20:55:27 -0300

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/TiziMass.py
@@ -0,0 +1,18 @@
+# https://www.python.org/
+
+#
+
+Messi = 10
+PI = 3,1415
+HORAS_DIA = 24
+DIAS_SEMANA = 7
+
+Silla = "Bienvenidos"
+Silla = 10
+Silla = True
+Silla = 19.45
+Silla = "B"
+
+print("¡Hola Python")
+
+
