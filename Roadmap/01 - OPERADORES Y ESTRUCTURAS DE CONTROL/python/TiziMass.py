77b68dad40e41d6204d70f0d56040a24eb08975b
TiziMass <149125722+TiziMass@users.noreply.github.com>
2024-01-03 21:18:48 -0300

    #01 - Python

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/TiziMass.py
@@ -0,0 +1,48 @@
+#Aritméticos
+
+Suma = 3 + 5 #Suma
+Resta = 5 - 2 #Resta
+Multiplicacion = 3 * 4 #Multiplicacion
+Division = 6 / 3 #Division
+Potencia = 3 ** 2 #Potencia
+Modulo = 10 % 3 #Modulo
+
+print(Suma)
+print(Resta)
+print(Multiplicacion)
+print(Division)
+print(Potencia)
+print(Modulo)
+
+#Lógicos
+
+Mayor = 3 > 2 #Mayor
+Menor = 2 < 5 #Menor
+Logicas1 = (2 > 4 and 6 < 8) #AND
+Logicas2 = (4 > 2 or 6 < 2) #OR
+
+print(Mayor)
+print(Menor)
+print(Logicas1)
+print(Logicas2)
+
+#Asignacion
+
+Asignar1 = 3 == 4 #False
+Asignar2 = 4 == 4 #True
+
+print(Asignar1)
+print(Asignar2)
+
+#Pertenencia
+
+creadores = ["S4vitar", "Moure", "Baity", "Lord", "Grefg"]
+for Moure in creadores:
+    print(Moure) 
+
+S4vitar = 29 
+if S4vitar > 30:
+    print("Edad suficiente")
+else: 
+    print("Edad no suficiente")
+
